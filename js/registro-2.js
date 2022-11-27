348b46d77eed0c5644326ba1c09b933df2ec7d9c
David Granados <davidgranados4@gmail.com>
2022-11-27 03:57:23 +0100

    changes

--- js/registro-2.js
+++ js/registro-2.js
@@ -12,21 +12,21 @@ function leerCookie(nombre) {
 }
 
 $(document).ready(function () {
-    if($("#nombre_hermandad").val().length < 1
-    || $("#direccion").val().length < 1
-    || $("#anio").val().length < 1
-    || $("#numero_hermanos").val().length < 1
-    //ver como se escoje color
-    || $("#color").val().length < 1
-    || $("#telefono").val().length < 1
-    || $("#pago").val().length < 1){
+    if ($("#nombre_hermandad").val().length < 1
+        || $("#direccion").val().length < 1
+        || $("#anio").val().length < 1
+        || $("#numero_hermanos").val().length < 1
+        //ver como se escoje color
+        || $("#color").val().length < 1
+        || $("#telefono").val().length < 1
+        || $("#pago").val().length < 1) {
         errors = 1;
     }
     //Nombre Hermandad
-    $("#nombre_hermandad").blur(()=>{ 
-        if($("#nombre_hermandad").val().length < 1){
+    $("#nombre_hermandad").blur(() => {
+        if ($("#nombre_hermandad").val().length < 1) {
             $("#log_nombre_hermandad_regis").text("Esta vacio").css("color", "red");
-        }else{
+        } else {
             if (user.hermandad !== $("#nombre_hermandad").val()) {
                 $("#log_nombre_hermandad_regis").css("display", "none");
                 document.cookie = "hermandad=" + $("#nombre_hermandad").val();
@@ -35,6 +35,67 @@ $(document).ready(function () {
                 }
             } else {
                 $("#log_nombre_hermandad_regis").text("Hermandad ya registrado").css("color", "red");
+                errors++;
+            }
+        }
+    });
+
+    //Direccion
+    $("#direccion").blur(() => {
+        if ($("#direccion").val().length < 1) {
+            $("#log_direccion").text("Esta vacio").css("color", "red");
+        } else {
+            if (user.direccion !== $("#direccion").val()) {
+                $("#log_direccion").css("display", "none");
+                document.cookie = "direccion=" + $("#direccion").val();
+                if (errors != 0) {
+                    errors--;
+                }
+            } else {
+                $("#log_direccion").text("Direccion ya registrado").css("color", "red");
+                errors++;
+            }
+        }
+    });
+    //Anio creacion
+    $("#anio").blur(() => {
+        if ($("#anio").val().length < 1) {
+            $("#log_anio").text("Esta vacio").css("color", "red");
+        } else {
+            $("#log_anio").css("display", "none");
+            document.cookie = "anio=" + $("#anio").val();
+            if (errors != 0) {
+                errors--;
+            }
+        }
+    });
+    //Numero de hermanos
+    $("#numero_hermanos").blur(() => {
+        if ($("#numero_hermanos").val().length < 1) {
+            $("#log_number").text("Esta vacio").css("color", "red");
+        } else {
+            $("#log_number").css("display", "none");
+            document.cookie = "numero_hermanos=" + $("#anio").val();
+            if (errors != 0) {
+                errors--;
+            }
+        }
+    });
+    //Colores
+
+    //Telefono
+    $("#telefono").blur(() => {
+        if ($("#telefono").val().length < 1) {
+            $("#log_telefono").text("Esta vacio").css("color", "red");
+        } else {
+            if (user.telefono !== $("#telefono").val()) {
+                $("#log_telefono").css("display", "none");
+                document.cookie = "telefono=" + $("#telefono").val();
+                if (errors != 0) {
+                    errors--;
+                }
+            } else {
+                $("#log_telefono").text("Telefono ya registrado").css("color", "red");
                 errors++;
             }
         }
@@ -51,19 +112,18 @@ $(document).ready(function () {
 
 
 
-
     user.name = leerCookie("name");
     user.email = leerCookie("email");
     user.pass = leerCookie("pass");
-    localStorage.setItem("usuario",JSON.stringify(user));
-    
-        
-        
-    $("#btnEnviar").click(()=>{
-        
+    localStorage.setItem("usuario", JSON.stringify(user));
+
+
+
+    $("#btnEnviar").click(() => {
+
         $("#formulario").attr("action", "inicio.html");
     });
-    $("#btnAtras").click(()=>{
+    $("#btnAtras").click(() => {
         $("#formulario").attr("action", "registro.html");
     });
 });
